29f264e2b81aa17407c8e9ac46bc6df82171d718
bdolor <bpayne@bccampus.ca>
2018-09-25 15:52:09 -0700

    include file manually in absense of autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,6 +18,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/bc-validate.php';
+	require dirname( __DIR__ ) . '/public/class-bc-validate-public.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
